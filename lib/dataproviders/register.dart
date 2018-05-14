85e1e5c63f6cc7f4d10ed00a8b207b28ba65c3c4
Mike Pesate <>
2018-05-14 17:21:13 +0200

    Added placeholders for the email validation and registration service calls

---
+++ lib/dataproviders/register.dart
@@ -0,0 +1,11 @@
+class Register {
+
+  String _url = "https://api.dcoupon.eu/id/register";
+
+  Map<String, String> body = {"emailAddress":"ixxgznle@sharklasers.com","password":"password","appId":"dcoupondev","alias":"Nombre","gender":"MALE","dob":"2004-04-10","lang":"es","source":"https://es.dcoupon.eu/market/"};
+
+  Map<String, String> headers = {"Content-Type" : "application/json", "Authorization": "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzYWRhNmFhNC1lY2UyLTQyMjAtYTVkMi0xYjBiY2FhYWM5NmMiLCJpc3MiOiJkY291cG9uLmNvbSIsImlhdCI6MTUyNjI4ODYzMSwiZXhwIjoxNTU3ODI0NjMxLCJzdWIiOiIxODgyNyIsImVtYWlsIjoiZWVwc2Zldm1Ac2hhcmtsYXNlcnMuY29tIiwiYWxpYXMiOiJOb21icmUiLCJyZWd0eXAiOiJlbWFpbCJ9.O-bpKSLYbZpREejOqVy4ENFoRG-0F2Ove21SSroKS4w"};
+
+  Map<String, dynamic> response = {"statusCode":201,"message":"User created verify to confirm"};
+
+}
---
+++ lib/dataproviders/validateemail.dart
@@ -0,0 +1,12 @@
+class ValidateEmail {
+
+  String _url = "https://api.dcoupon.eu/id/validate";
+
+  Map<String, String> body = {"email":"eepsfevm@sharklasers.com","registrationType":"EMAIL"};
+
+  Map<String, dynamic> badResponse = {"responseCode": 101,"error": "ERROR.CONFLICT"};
+  Map<String, dynamic> goodResponse = {"responseCode":105,"error":"EMAIL.NOT.IN.USE"};
+
+  Map<String, String> headers = {"Content-Type" : "application/json", "Authorization": "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzYWRhNmFhNC1lY2UyLTQyMjAtYTVkMi0xYjBiY2FhYWM5NmMiLCJpc3MiOiJkY291cG9uLmNvbSIsImlhdCI6MTUyNjI4ODYzMSwiZXhwIjoxNTU3ODI0NjMxLCJzdWIiOiIxODgyNyIsImVtYWlsIjoiZWVwc2Zldm1Ac2hhcmtsYXNlcnMuY29tIiwiYWxpYXMiOiJOb21icmUiLCJyZWd0eXAiOiJlbWFpbCJ9.O-bpKSLYbZpREejOqVy4ENFoRG-0F2Ove21SSroKS4w"};
+
+}
